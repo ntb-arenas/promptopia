833555c4825473f232f4744da47a4c7bd9e97bf1
Niño Arenas <ntb.arenas@protonmail.com>
2023-06-30 17:12:15 +0100

    update api

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Feed from "@components/Feed";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  return (
+    <section className="flex-col w-full flex-center">
+      <h1 className="text-center head_text">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="text-center orange_gradient">AI-powered Prompts</span>
+      </h1>
+      <p className="text-center desc">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discvoer, create and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
 };
 
-export default page;
+export default Home;
