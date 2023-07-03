b9baa4e4a2ad1d2944524dbd4b26a32371c906fa
Niño Arenas <ntb.arenas@protonmail.com>
2023-07-03 13:23:55 +0100

    update prompt model db

--- models/prompt.js
+++ models/prompt.js
@@ -1,24 +1,20 @@
 import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    unique: [true, "Email already exists!"],
-    required: [true, "Email is required!"],
+const PromptSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
   },
-
-  username: {
+  prompt: {
     type: String,
-    required: [true, "Username is required!"],
-    match: [
-      /^[A-Za-z0-9_ñ][A-Za-z0-9_ ñ]{3,28}$/,
-      "Username invalid, it should contain 4-29 alphanumeric letters, underscores, or spaces and be unique!",
-    ],
+    required: [true, "Prompt is required."],
+    tag: {
+      type: String,
+      requiredPaths: [true, "Tag is required"],
+    },
   },
-
-  image: { type: String },
 });
 
-const User = models.User || model("User", UserSchema);
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
 
-export default User;
+export default Prompt;
