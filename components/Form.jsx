51215d8f5ded9a0f30a0efc4e8e32aed22fe57f1
Niño Arenas <ntb.arenas@protonmail.com>
2023-07-03 13:16:07 +0100

    update Form component & create-prompt page

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import Link from "next/link";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="flex-col w-full max-w-full flex-start">
+      <h1 className="text-left head_text">
+        <span className="blue_gradient">{type}</span> Post
+      </h1>
+
+      <p className="max-w-md text-left desc">
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-power platform.
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col w-full max-w-2xl mt-10 gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-semibold text-gray-700 font-satoshi text-base-">
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+
+        <label>
+          <span className="font-semibold text-gray-700 font-satoshi text-base-">
+            Tag{" "}
+            <span className="font-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          ></input>
+        </label>
+
+        <div className="gap-4 mx-3 mb-5 flex-end">
+          <Link href={"/"} className="text-sm text-gray-500">
+            Cancel
+          </Link>
+
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
